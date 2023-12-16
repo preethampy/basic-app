54d311596510ea5cc5820b8f29ef715b88db7c1c
preetham-interakt <preetham.interakt@gmail.com>
2023-12-16 14:12:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Create a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
